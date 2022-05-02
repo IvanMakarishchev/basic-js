70ceabefc745b79b702e8fdcb31d15a66de61c2b
IvanMakarishchev <denis.flank@gmail.com>
2022-05-02 04:49:27 +0300

    feat transform array***

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let transArray = [];
+  if (arr instanceof Array) {
+    for (let i = 0; i < arr.length; i++){
+      arrElement = +arr[i]
+      if (typeof arrElement === 'number' && !isNaN(arrElement)) {
+        transArray.push(arrElement);
+      } else {
+        if (i > 0 && typeof +arr[i-1] === 'number' && !isNaN(arrElement)){
+          switch (arr[i]) {
+            case '--discard-next':
+              i < arr.length-2 && i++;
+              break;
+            case '--discard-prev':
+              i > 0 && transArray.length > 0 && transArray.pop();
+              break;
+            case '--double-next':
+              i < arr.length-1 && transArray.push(+arr[i+1]*2);
+              i++;
+              break;
+            case '--double-prev':
+              (i > 0 && transArray.length > 0) ? transArray[i-1]*=2 : ''
+              break;
+            case NaN:
+              break;
+          }
+        }
+      }
+    }
+    return transArray;
+  } else {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  }
+  
 }
+
 
 module.exports = {
   transform
